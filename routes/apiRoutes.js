3fb327b5b970bef7b85840f634ba820678c5ab2c
Corbin Garner <Corbingar@gmail.com>
2021-06-05 16:51:01 -0700

    finished both put routes, suprisingly similar is sql

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,8 +10,8 @@ router.put("/api/workouts/:id", (req, res) => {
 
 
         .then(data => {
-            res.json(data);
-        })
+    res.json(data);
+})
         .catch(err => {
             res.status(500).json(err);
         });
@@ -22,7 +22,15 @@ router.put("/api/workouts/:id", (req, res) => {
 
 //addExercise
 router.put("/api/workouts/:id", (req, res) => {
-
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration',
+                },
+            },
+        },
+    ])
 
         .then(data => {
             res.json(data);
@@ -31,14 +39,15 @@ router.put("/api/workouts/:id", (req, res) => {
             res.status(500).json(err);
         });
 
-}
+});
 
 
 //createWorkout
 router.put("/api/workouts/:id", (req, res) => {
-
-
-
+    Workout.findByIdAndUpdate(
+        req.params.id,
+        { $push: { exercises: req.body } },
+    )
         .then(data => {
             res.json(data);
         })
